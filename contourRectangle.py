abe61050f927aa09b7dc5f7a28069d7eac6d355a
Bernhard Klein <berndklein@gmx.de>
2018-10-21 12:22:28 +0200

    bug fix - CW & CCW implementation and a reimplementing generateGCode

--- contourRectangle.py
+++ contourRectangle.py
@@ -88,12 +88,12 @@ class ContourRectangle(GeometricalFrame):
 
         row += 1
         self.__dir = StringVar()
-        self.__dir.set("G02")
+        self.__dir.set("0")
         Label(self.frmButtonsIndividualContent, text='Contour direction').grid(
             row=row, column=0, sticky=W)
-        Radiobutton(self.frmButtonsIndividualContent, text="CW (G02)", variable=self.__dir,
-                    value="G02").grid(row=row, column=1, sticky=W)
-        Radiobutton(self.frmButtonsIndividualContent, text="CCW (G03)", variable=self.__dir,
+        Radiobutton(self.frmButtonsIndividualContent, text="CW", variable=self.__dir,
+                    value=0).grid(row=row, column=1, sticky=W)
+        Radiobutton(self.frmButtonsIndividualContent, text="CCW", variable=self.__dir,
                     value=1).grid(row=row, column=2, sticky=W)
 
         row += 1
@@ -124,15 +124,15 @@ class ContourRectangle(GeometricalFrame):
             textvariable=self.__centerY).grid(row=row, column=3, sticky=W)
 
         row += 1
-        self.__heightA = StringVar(value="20.0")
-        self.__widthB = StringVar(value="30.0")
+        self.__distanceA = StringVar(value="20.0")
+        self.__distanceB = StringVar(value="30.0")
         Label(self.frmButtonsIndividualContent, text="Height A").grid(row=row, column=0, sticky=W)
         Label(self.frmButtonsIndividualContent, text="Width B").grid(row=row, column=2, sticky=W)
         FloatEntry(self.frmButtonsIndividualContent, width=5,
-            textvariable=self.__heightA).grid(
+            textvariable=self.__distanceA).grid(
             row=row, column=1, sticky=W)
         FloatEntry(self.frmButtonsIndividualContent, width=5,
-            textvariable=self.__widthB).grid(row=row, column=3, sticky=W)
+            textvariable=self.__distanceB).grid(row=row, column=3, sticky=W)
 
         row += 1
         self.__depthtotal = StringVar(value="-0.5")
@@ -198,14 +198,30 @@ class ContourRectangle(GeometricalFrame):
     # insert your code bettween marked rows
     #-------------------------------------------------------------
     def generateGCode(self):
-        X = float(0.0)
-        Y = float(0.0)
-        Z = float(0.0)
+        cPoint = (
+            float(self.__centerX.get()),
+            float(self.__centerY.get())
+        )
+        zPos = {
+            "safetyZ"   : float(self.__safety_Z.get()),
+            "startZ"    : float(self.__start_Z.get())
+        }
 
-        xoffset = float(0.0)
-        yoffset = float(0.0)
-        a = float(self.__heightA.get())
-        b = float(self.__widthB.get())
+        sizeAB = (
+            float(self.__distanceA.get()),
+            float(self.__distanceB.get())
+        )
+
+        feeds = {
+            "XYG0" : float(self.__speed_XY_G00.get()),
+            "XYGn" : float(self.__speed_Z_G01.get()),
+            "ZG0" : float(self.__speed_Z_G00.get()),
+            "ZGn" : float(self.__speed_Z_G01.get())
+        }
+
+
+        # X/Y
+        cutterComp = (0.0, 0.0)
 
         gc = ""
         # Preamble
@@ -216,108 +232,134 @@ class ContourRectangle(GeometricalFrame):
         # set Z axis
         gc += CR + "(set Z saftey position)" + CR
         gc += "G00 Z{0:08.3f} F{1:05.1f} {2}".format(
-            float(self.__safety_Z.get()),
-            float(self.__speed_Z_G00.get()), CR)
+            zPos["safetyZ"], feeds["ZG0"], CR)
 
         #
         # even which center point user choosed, we start on
         # center point object - left down (5)
 
-        # mill left down
+        # User used left down corner as CP
         if (int(self.__CC.get()) == 1):
-            X = float(self.__centerX.get())
-            Y = float(self.__centerY.get())
-        # mill left upper
+            cPoint = (
+                float(self.__centerX.get()),
+                float(self.__centerY.get())
+            )
+        # User used left upper corner as CP
         if (int(self.__CC.get()) == 2):
-            X = float(self.__centerX.get())
-            Y = -float(self.__centerY.get())
-        # mill right upper
+            cPoint = (
+                float(self.__centerX.get()),
+                float(self.__centerY.get()) - sizeAB[0]
+            )
+        # User used right upper corner as CP
         if (int(self.__CC.get()) == 3):
-            X = -float(self.__centerX.get())
-            Y = -float(self.__centerY.get())
-        # mill right down
+            cPoint = (
+                float(self.__centerX.get()) - sizeAB[1],
+                float(self.__centerY.get()) - sizeAB[0]
+            )
+        # User used right down corner as CP
         if (int(self.__CC.get()) == 4):
-            X = -float(self.__centerX.get())
-            Y = float(self.__centerY.get())
+            cPoint = (
+                float(self.__centerX.get()) - sizeAB[1],
+                float(self.__centerY.get())
+            )
 
         # object left down - this is our real center point
         if (int(self.__CC.get()) == 5):
-            X = float(0.0)
-            Y = float(0.0)
+            cPoint = (
+                0.0 - sizeAB[1],
+                0.0
+            )
         # object left upper
         if (int(self.__CC.get()) == 6):
-            X = float(0.0)
-            Y = float(0.0) - a
+            cPoint = (
+                0.0 ,
+                0.0 - sizeAB[0]
+            )
         # object right upper
         if (int(self.__CC.get()) == 7):
-            X = float(0.0) - b
-            Y = float(0.0) - a
+            cPoint = (
+                0.0 - sizeAB[1],
+                0.0 - sizeAB[0]
+            )
         # object right down
         if (int(self.__CC.get()) == 8):
-            X = float(0.0) - b
-            Y = float(0.0)
+            cPoint = (
+                0.0 - sizeAB[1],
+                0.0
+            )
         # object center
         if (int(self.__CC.get()) == 9):
             print "cc 9"
-            X += float(0.0) - (b/2.0)
-            Y = float(0.0) - (a/2.0)
+            cPoint = (
+                0.0 - (sizeAB[1] / 2.0),
+                0.0 - (sizeAB[0] / 2.0)
+            )
 
         # cutter compensation
-        ccX = 0.0
-        ccY = 0.0
         if (self.__cuttercompensation.get() == "G40"):
             gc += CR + "(-- Cutter compensation --){}".format(CR)
             gc += "{} {}".format(self.__cuttercompensation.get(),CR)
+            # nothing to do with cutterComp
+            cutterComp = (0.0, 0.0)
         if (self.__cuttercompensation.get() == "G41"):
             gc += CR + "(-- Cutter compensation LEFT --){}".format(CR)
             gc += "{} {}".format(self.__cuttercompensation.get(),CR)
-            ccX = -(float(self.__tooldia) / 2.0)
-            ccY = -(float(self.__tooldia) / 2.0)
+            cutterComp = (
+                -(float(self.__tooldia.get()) / 2.0),
+                -(float(self.__tooldia.get()) / 2.0)
+            )
         if (self.__cuttercompensation.get() == "G42"):
             gc += CR + "(-- Cutter compensation RIGHT --){}".format(CR)
             gc += "{} {}".format(self.__cuttercompensation.get(),CR)
-            ccX = (float(self.__tooldia) / 2.0)
-            ccY = (float(self.__tooldia) / 2.0)
+            cutterComp = (
+                (float(self.__tooldia.get()) / 2.0),
+                (float(self.__tooldia.get()) / 2.0)
+            )
 
-        if (self.__dir.get() == "G03"):
-            # mill run CCW
-            ccX *= -1
-            ccY *= -1
+        dir = int(self.__dir.get())
+        if (dir == 0):
+            dirS = "CW"
+        else:
+            dirS = "CCW"
 
-        X += ccX
-        Y += ccY
+        cPoint = (
+            cPoint[0] + cutterComp[0],
+            cPoint[1] + cutterComp[1],
+        )
 
         #
         # generate as many shape steps are needed until depthtotal is reached
         # cut an Arc
         step = float(self.__depthstep.get())
         depth = float(self.__depthtotal.get())
-        z = step
+        z = 0.0
         loop = ""
         gc += CR + "(------- start shape -------------)" + CR
         gc += "(-- A {0:06.3f}, B {1:06.3f}, Depth {2:06.3f}, Step {3:06.3f} --){4}".format(
-            float(a),
-            float(b),
+            sizeAB[0],
+            sizeAB[1],
             depth,
             step,
             CR
         )
         gc += "(-- X {0:06.3f}, Y {1:06.3f} --) {2}".format(
-            float(X),
-            float(Y),
+            cPoint[0],
+            cPoint[1],
             CR
         )
         # start with shape
         gc += CR + "(move Z-axis to start postion near surface)" + CR
         gc += "G00 Z{0:08.3f} F{1:05.1f} {2}".format(
-            float(self.__start_Z.get()),
-            float(self.__speed_Z_G00.get()), CR)
+            zPos["startZ"],
+            feeds["ZG0"],
+            CR)
 
         # set start postion X/Y
+        gc += CR + "(we allways start at lower left corner)" + CR
         gc += "G00 X{0:08.3f} Y{1:08.3f} F{2:05.1f} {3}".format(
-            float(X),
-            float(Y),
-            float(self.__speed_XY_G00.get()),
+            cPoint[0],
+            cPoint[1],
+            feeds["XYG0"],
             CR)
 
 
@@ -326,7 +368,8 @@ class ContourRectangle(GeometricalFrame):
         # if not needed for your shape, remove this part and rewrite
         #----------------------------------------------------------------------
         #
-        gc += CR + "(-- loop --)" + CR
+        gc += CR + "(-- loop in {} --)".format(dirS) + CR
+        gc += CR + "(-- Z {0:08.3f} --)".format(z) + CR
         # start with shape
 
         # set start postion X/Y
@@ -342,8 +385,8 @@ class ContourRectangle(GeometricalFrame):
 
             #
             # start with this start position
-            loop += self.__createGCodeRect(X,Y,Z,a,b)
-
+            #loop += self.__createGCodeRect(dir, X,Y,Z,a,b)
+            loop += self.__createGCodeRect(dir, cPoint, z, sizeAB, feeds)
             #---------------------------------------------------
             # indiviual GCode - END
             #---------------------------------------------------
@@ -360,39 +403,74 @@ class ContourRectangle(GeometricalFrame):
         #print gc
         return  gc
 
-    def __createGCodeRect(self, x,y,z,a,b):
+    def __createGCodeRect(self, dir, cPoint,z,sizeAB, feeds):
         gc = ""
         gc += CR + "(set new Z {0:05.2f} position)".format(z) + CR
         gc += "G00 Z{0:08.3f} F{1:05.1f} {2}".format(
             float(z),
-            float(self.__speed_Z_G00.get()), CR)
+            feeds["ZGn"],
+            CR)
 
-        # go to left up
-        gc += "G01 X{0:08.3f} Y{1:08.3f} Z{2:08.3f} F{3:05.1f} {4}".format(
-                float(x),
-                float(y + a),
-                float(z),
-                float(self.__speed_XY_G02G03.get()),
-                CR)
-        # go right up
-        gc += "G01 X{0:08.3f} Y{1:08.3f} Z{2:08.3f} F{3:05.1f} {4}".format(
-                float(x + b),
-                float(y + a),
-                float(z),
-                float(self.__speed_XY_G02G03.get()),
-                CR)
-        # go right down
-        gc += "G01 X{0:08.3f} Y{1:08.3f} Z{2:08.3f} F{3:05.1f} {4}".format(
-                float(x),
-                float(y-b),
-                float(z),
-                float(self.__speed_XY_G02G03.get()),
-                CR)
-        # back to start (left down)
-        gc += "G01 X{0:08.3f} Y{1:08.3f} Z{2:08.3f} F{3:05.1f} {4}".format(
-                float(x),
-                float(y),
-                float(z),
-                float(self.__speed_XY_G02G03.get()),
-                CR)
+        # Positions
+        # 2-------------3
+        # |             |
+        # |             |
+        # |             |
+        # 1-------------4
+        # start is #1
+        millPos = {
+            "#1" : "G01 X{0:08.3f} Y{1:08.3f} Z{2:08.3f} F{3:05.1f} {4}".format(
+                    cPoint[0],
+                    cPoint[1],
+                    z,
+                    feeds["XYGn"],
+                    CR),
+            "#2" : "G01 X{0:08.3f} Y{1:08.3f} Z{2:08.3f} F{3:05.1f} {4}".format(
+                    cPoint[0],
+                    cPoint[1] + sizeAB[0],
+                    float(z),
+                    feeds["XYGn"],
+                    CR),
+            "#3" : "G01 X{0:08.3f} Y{1:08.3f} Z{2:08.3f} F{3:05.1f} {4}".format(
+                    cPoint[0] + sizeAB[1],
+                    cPoint[1] + sizeAB[0],
+                    float(z),
+                    feeds["XYGn"],
+                    CR),
+            "#4" : "G01 X{0:08.3f} Y{1:08.3f} Z{2:08.3f} F{3:05.1f} {4}".format(
+                    cPoint[0] + sizeAB[1],
+                    cPoint[1],
+                    float(z),
+                    feeds["XYGn"],
+                    CR)
+        }
+        # start on #1
+        gc += millPos["#1"]
+        # to #2 / #4
+        if (dir == 0):
+            # CW
+            gc += millPos["#2"]
+        else:
+            # CCW
+            gc += millPos["#4"]
+
+        # to #3 / #3
+        if (dir == 0):
+            # CW
+            gc += millPos["#3"]
+        else:
+            # CCW
+            gc += millPos["#3"]
+
+        # to #4 / #2
+        if (dir == 0):
+            # CW
+            gc += millPos["#4"]
+        else:
+            # CCW
+            gc += millPos["#2"]
+
+        # back on #1
+        gc += millPos["#1"]
+
         return gc
